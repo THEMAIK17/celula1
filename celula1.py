71975a5bdeb2e3fc7bb4aec61788e0c2f8fdb7f3
gabrielayanesp <gabriela.yanesp17@gmail.com>
2025-04-04 19:01:15 -0500

    Corregi algunos errores de espaciado y mejore un poco la interfaz visual

--- celula1.py
+++ celula1.py
@@ -2,25 +2,25 @@
 
 preguntas = [
     {
-        "pregunta": "¿Cuál de los siguientes personas fue un filosofo?",
-        "opciones": ["A. Albert Einstein ", "B. George Washington ", "C. Platon", "D. William Shakespeare"],
+        "pregunta": "\n¿Cuál de los siguientes personas fue un filosofo?",
+        "opciones": ["\nA. Albert Einstein ", "B. George Washington ", "C. Platon", "D. William Shakespeare\n"],
         "respuesta_correcta": "C"
     },
     {
-        "pregunta": "¿Cual de las siguientes es una pregunta filosofica fundamental?",
-        "opciones": ["A. ¿Cuál es el nombre del presidente actual de Colombia?", "B. ¿Qué es la felicidad?", "C. ¿Cuál es el precio de una libra de arroz?", "D. ¿Qué hora es? "],
+        "pregunta": "\n¿Cual de las siguientes es una pregunta filosofica fundamental?",
+        "opciones": ["\nA. ¿Cuál es el nombre del presidente actual de Colombia?", "B. ¿Qué es la felicidad?", "C. ¿Cuál es el precio de una libra de arroz?", "D. ¿Qué hora es? \n"],
         "respuesta_correcta": "B"
     },
     {
-        "pregunta": "¿Qué es la ética, según la filosofía clásica?", 
-        "opciones": ["A. El estudio de la belleza y el arte.", "B. El estudio del conocimiento y la verdad. ", "C.  El estudio del bien y el mal.", "D. El estudio de la existencia y el universo"],
+        "pregunta": "\n¿Qué es la ética, según la filosofía clásica?", 
+        "opciones": ["\nA. El estudio de la belleza y el arte.", "B. El estudio del conocimiento y la verdad. ", "C. El estudio del bien y el mal.", "D. El estudio de la existencia y el universo\n"],
         "respuesta_correcta": "C"
     }
 ]
 contador = 0
-print ("="*25)
-print ("     ENCUESTA RIWI     ")
-print ("="*25)
+print ("="*64)
+print ("                       QUIZ DE FILOSOFIA    ")
+print ("="*64)
 
 # Mostrar las preguntas y opciones
 for pregunta in preguntas:
